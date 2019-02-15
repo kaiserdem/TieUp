8ae06f833030772a3d69c5982dcf15260a38bc0a
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-02-15 22:38:09 +0200

    first commit init project

---
+++ TieUp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TieUp
+//
+//  Created by Kaiserdem on 15.02.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ TieUp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TieUp
+//
+//  Created by Kaiserdem on 15.02.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
