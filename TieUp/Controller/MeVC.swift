9e2cde5c6709f73b94f3bc1cf0dee3633da0a50c
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-02-19 20:24:51 +0200

    add MeVC and create screen

---
+++ TieUp/Controller/MeVC.swift
@@ -0,0 +1,25 @@
+//
+//  MeVC.swift
+//  TieUp
+//
+//  Created by Kaiserdem on 19.02.2019.
+//  Copyright © 2019 Kaiserdem. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var emailLable: UILabel!
+  @IBOutlet weak var profileImageView: UIImageView!
+  override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  @IBAction func singOutBtnPressed(_ sender: Any) {
+  }
+}
